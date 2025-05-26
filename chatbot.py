b97418581a826794287314c2b4c54e634f7b0dcf
EberronBruce <bruce@redravencomputing.com>
2025-05-26 14:48:17 -0400

    complete the basic chatbot

--- chatbot.py
+++ chatbot.py
@@ -8,23 +8,22 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 #Add variable to keep track of history
 conversation_history = []
-#Make a string of the history
-history_string = "\n".join(conversation_history)
-#Fetch prompt from the user
-input_text ="hello, how are you doing?"
-#Tokenization of user prompt and chat history
-inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
-print(inputs)
 
-tokenizer.pretrained_vocab_files_map
+while True:
+    #Make a string of the history
+    history_string = "\n".join(conversation_history)
+    #Fetch prompt from the user
+    input_text = input("> ")
+    #Tokenization of user prompt and chat history
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
-outputs = model.generate(**inputs)
-print(outputs)
+    tokenizer.pretrained_vocab_files_map
 
-response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-print(response)
+    outputs = model.generate(**inputs)
 
-conversation_history.append(input_text)
-conversation_history.append(response)
-print(conversation_history)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    print(response)
+
+    conversation_history.append(input_text)
+    conversation_history.append(response)
 
